f55fe956fda6dbe763b29ea96b8199ba9e8f17f2
MinhDuc <duc11t3bk@gmail.com>
2020-02-09 22:14:28 +0900

    init

---
+++ src/main/java/webhelloworld/WebHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package webhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebHelloWorldApplication.class, args);
+	}
+
+}
